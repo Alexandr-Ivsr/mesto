cc50d465e9309c5e0339e7096a5bf45f58d76654
Alexandr Ivanov <Kurotsuchi346@yandex.ru>
2021-10-09 00:52:49 +0300

    Updates

--- src/utils/utils.js
+++
@@ -1,8 +0,0 @@
-import { nameInput, aboutInput } from './constants.js';
-import { userInfo } from '../pages/index.js';
-
-export function getProfileValues() {
-  const userInfoValues = userInfo.getUserInfo();
-  nameInput.value = userInfoValues.userName;
-  aboutInput.value = userInfoValues.userInfo;
-}
